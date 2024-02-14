af705d9f8f28194c2866b3713edd30eaf12c7738
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-13 18:28:25 -0600

    used bs4 to get html out of f1

--- f1_scraper_play_ground.py
+++ f1_scraper_play_ground.py
@@ -1,4 +1,10 @@
 #%%
 import pandas as pd
 from bs4 import BeautifulSoup
+import requests
 #%%
+url = 'https://www.formula1.com/en/results.html/2023/races.html'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+links = soup.find_all('a')
+print(links)
