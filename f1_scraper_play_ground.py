2dec5c58484f34c98684eb971a15f937d91ec963
vamsee9201 <vamseekrishna9201@gmail.com>
2024-02-15 22:10:44 -0600

    created driver table

--- f1_scraper_play_ground.py
+++ f1_scraper_play_ground.py
@@ -3,14 +3,31 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 #%%
-url = 'https://www.formula1.com/en/results.html/2023/races.html'
+url = 'https://www.formula1.com/en/results.html/2023/drivers.html'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 links = soup.find_all('a')
-print(links)
 #%%
-links = soup.find_all({"class":"clip"})
-links
+firstNames = [ span.get_text() for span in soup.find('tbody').find_all(class_ = "hide-for-tablet")]
 # %%
-links1 = soup.find_all({"class":"clip"})
-links1
+lastNames = [ span.get_text() for span in soup.find('tbody').find_all(class_ = "hide-for-mobile")]
+# %%
+driverCodes = [ span.get_text() for span in soup.find('tbody').find_all(class_ = "uppercase hide-for-desktop")]
+# %%
+driverNationalities = [ span.get_text() for span in soup.find('tbody').find_all(class_ = "dark semi-bold uppercase")]
+# %%
+driverPoints =  [ span.get_text() for span in soup.find('tbody').find_all(class_ = "dark bold")]
+# %%
+dataFrameJson = {
+    "Code":driverCodes,
+    "firstName":firstNames,
+    "lastName":lastNames,
+    "Nationality":driverNationalities,
+    "points":driverPoints
+}
+# %%
+driverTable = pd.DataFrame(dataFrameJson)
+
+# %%
+driverTable
+# %%
