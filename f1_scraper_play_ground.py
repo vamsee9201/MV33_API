6bb955805da4a93f5443bd74fe463938bd02e78a
vamsee9201 <vamseekrishna9201@gmail.com>
2024-03-07 11:09:44 -0600

    created convert dates function

--- f1_scraper_play_ground.py
+++ f1_scraper_play_ground.py
@@ -3,6 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import time
+from datetime import datetime
 #%%
 url = 'https://www.formula1.com/en/results.html/2023/drivers.html'
 response = requests.get(url)
@@ -109,8 +110,104 @@ def getSchedule(year):
     }
     return scheduleJson
 #%%
-getSchedule(2024)
+startDatesSample = ['29 Feb',
+  '07',
+  '22',
+  '05',
+  '19',
+  '03',
+  '17',
+  '24',
+  '07',
+  '21',
+  '28',
+  '05',
+  '19',
+  '26',
+  '23',
+  '30 Aug',
+  '13',
+  '20',
+  '18',
+  '25',
+  '01',
+  '21',
+  '29 Nov',
+  '06']
+endDatesSample = ['02 Mar 2024',
+  '09 Mar 2024',
+  '24 Mar 2024',
+  '07 Apr 2024',
+  '21 Apr 2024',
+  '05 May 2024',
+  '19 May 2024',
+  '26 May 2024',
+  '09 Jun 2024',
+  '23 Jun 2024',
+  '30 Jun 2024',
+  '07 Jul 2024',
+  '21 Jul 2024',
+  '28 Jul 2024',
+  '25 Aug 2024',
+  '01 Sep 2024',
+  '15 Sep 2024',
+  '22 Sep 2024',
+  '20 Oct 2024',
+  '27 Oct 2024',
+  '03 Nov 2024',
+  '23 Nov 2024',
+  '01 Dec 2024',
+  '08 Dec 2024']
 
+#%%
+def convertDates(startDates,endDates):
+    startDatesUpdated  = []
+    for index,date in enumerate(startDates):
+        if len(date) == 2:
+            date = date + " " + endDates[index][3:]
+        else :
+            date = date + " " +endDates[index][7:]
+        startDatesUpdated.append(date)
+    #converting to datetime
+    format = "%d %b %Y"
+    parsedStartDates = []
+    parsedEndDates = []
+    for date in startDatesUpdated:
+        parsedStartDate = datetime.strptime(date, format)
+        parsedStartDates.append(parsedStartDate)
+    for date in endDates:
+        parsedEndDate = datetime.strptime(date, format)
+        parsedEndDates.append(parsedEndDate)
 
-
+    return (parsedStartDates,parsedEndDates)
+# %%
+def getSchedule(year):
+    url = "https://www.formula1.com/en/results.html/{}/races.html".format(year)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    elements = soup.find('select',{"class":"resultsarchive-filter-form-select","name":"meetingKey"})
+    elements = elements.find_all('option')
+    elements = elements[1:]
+    links = [element.get("value") for element in elements]
+    circuitInfos = []
+    startDates = []
+    endDates = []
+    for link in links:
+        url = "https://www.formula1.com/en/results.html/{}/races/{}/race-result.html".format(year,link)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        circuitInfo = soup.find('span',{"class":"circuit-info"}).text
+        circuitInfos.append(circuitInfo)
+        startDate = soup.find('span',{"class":"start-date"}).text
+        startDates.append(startDate)
+        endDate = soup.find('span',{"class":"full-date"}).text
+        endDates.append(endDate)
+        time.sleep(1)
+    startDates,endDates = convertDates(startDates,endDates)
+    scheduleJson = {
+        "startDate":startDates,
+        "endDate":endDates,
+        "circuitInfo":circuitInfos
+    }
+    return scheduleJson
 # %%
