eec9c866d2208a3a90a36129a615f3c47782322a
vamsee9201 <vamseekrishna9201@gmail.com>
2024-04-30 22:19:55 -0500

    getting particular race details data

--- f1_scraper_play_ground.py
+++ f1_scraper_play_ground.py
@@ -254,8 +254,10 @@ temp = table.find("tbody")
 # %%
 temp
 #%%
-temp.find_all("span",{"class":"hide-for-tablet"})
-temp.find_all("span",{"class":"hide-for-mobile"})
+firstNames = [span.get_text() for span in temp.find_all("span",{"class":"hide-for-tablet"})]
+#%%
+lastNames = [span.get_text() for span in temp.find_all("span",{"class":"hide-for-mobile"})]
+#%%
 temp.find_all("td",{"class":"semi-bold uppercase hide-for-tablet"})
 temp.find_all("td",{"class":"bold hide-for-mobile"})
 temp.find_all("span",{"class":"uppercase hide-for-desktop"})
